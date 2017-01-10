4b4b10bab61d8181c69965de9f8a202f641d9023
igorGalchenko <igalchenko@speroteck.com>
2017-01-10 19:33:10 +0200

    south coast new theme for StoreFront

---
+++ app/pdp/_view.controller.js
@@ -0,0 +1,48 @@
+angular.module("pdpModule")
+
+    .controller("pdpViewController", [
+        '$controller',
+        "$scope",
+        "$routeParams",
+        "$location",
+        "$timeout",
+        "pdpApiService",
+        "pdpProductService",
+        "cartService",
+        "visitorLoginService",
+        "commonUtilService",
+        function ($controller, $scope, $routeParams, $location, $timeout, pdpApiService, pdpProductService,
+                  cartService, visitorLoginService, commonUtilService) {
+
+            // Inherit from default pdpViewController
+            $controller('_pdpViewController', { $scope: $scope });
+
+            $scope.imageWidget = {
+                active: {}
+            };
+
+            $scope._getProduct = function () {
+                pdpApiService.getProduct({"productID": $scope.productId}).$promise.then(function (response) {
+                    if (response.error === null) {
+                        var result = response.result || defaultProduct();
+
+                        pdpProductService.setProduct(result);
+                        $scope.product = pdpProductService.getProduct();
+
+                        // BREADCRUMBS
+                        $scope.$emit("add-breadcrumbs", {
+                            "label": $scope.product.name,
+                            "url": pdpProductService.getUrl($scope.product._id)
+                        });
+                    } else {
+                        $location.path("/");
+                    }
+                })
+                    .then(function(){
+                        // Activate the first image
+                        $scope.imageWidget.active = $scope.product.images[0];
+                    });
+            }
+        }
+    ]
+);
