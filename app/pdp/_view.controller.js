03a63355e22c78f2467900b8bf130acb2eceda2f
Dmitry Glushenkov <dglushenkov@speroteck.com>
2017-03-06 13:42:31 -0500

    fixing sold out label on pdp

--- app/pdp/_view.controller.js
+++ app/pdp/_view.controller.js
@@ -29,6 +29,9 @@ angular.module("pdpModule")
                         pdpProductService.setProduct(result);
                         $scope.product = pdpProductService.getProduct();
 
+                        // Inventory
+                        $scope.isProductInStock = $scope.product.qty === undefined || $scope.product.qty > 0;
+
                         // BREADCRUMBS
                         $scope.$emit("add-breadcrumbs", {
                             "label": $scope.product.name,
